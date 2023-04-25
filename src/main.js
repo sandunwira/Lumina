55b6aede9c29b9825d1825d9650040c6556277c6
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-04-25 13:50:19 +0530

    Simple web page

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 const { invoke } = window.__TAURI__.tauri;
+
+const countButton = document.getElementById("count");
+let clickCount = 0;
+
+countButton.addEventListener("click", () => {
+	clickCount++;
+	countButton.innerHTML = `Click Count: <b>${clickCount}</b>`;
+});
