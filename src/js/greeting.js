b078bc59360658668c484be2dad7f1247165ca65
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-05-06 19:05:01 +0530

    Added File Access and edited template

--- src/js/greeting.js
+++ src/js/greeting.js
@@ -5,18 +5,18 @@ function updateGreeting() {
 	const hours = now.getHours();
 
 	if (hours < 12) {
-		greetingDiv.innerHTML = "<h1>Good Morning</h1>";
+		greetingDiv.innerHTML = "<h1>Good Morning <i class='fa-solid fa-mug-saucer'></i></h1>";
 	} else if (hours < 16) {
-		greetingDiv.innerHTML = "<h1>Good Afternoon</h1>";
+		greetingDiv.innerHTML = "<h1>Good Afternoon <i class='fa-solid fa-sun'></i></h1>";
 	} else if (hours < 19) {
-		greetingDiv.innerHTML = "<h1>Good Evening</h1>";
+		greetingDiv.innerHTML = "<h1>Good Evening <i class='fa-solid fa-umbrella-beach'></i></h1>";
 	} else {
-		greetingDiv.innerHTML = "<h1>Good Night</h1>";
+		greetingDiv.innerHTML = "<h1>Good Night <i class='fa-solid fa-moon'></i></h1>";
 	}
 }
 
 // Update the greeting immediately when the page loads
 updateGreeting();
 
-// Update the greeting every hour
-setInterval(updateGreeting, 3600000);
+// Update the greeting every second
+setInterval(updateGreeting, 1000);
